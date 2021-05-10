dae5f26b3b22a77fb57a32ad36a24b124603a3ee
hareesh-r <hareeshprogrammer@gmail.com>
2021-05-10 19:22:46 +0530

    Material UI init

--- google-clone/src/pages/Home.js
+++ google-clone/src/pages/Home.js
@@ -4,7 +4,17 @@ import "./Home.css";
 function Home() {
     return (
         <div className="home">
-            
+            <div className="home__header">
+                <div className="homeHeardeLeft">
+
+                </div>
+                <div className="homeHeardeRight">
+                    
+                </div>
+            </div>
+            <div className="home__body">
+                Body
+            </div>
         </div>
     )
 }
