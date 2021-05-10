3d9c8620f6642e33239cec900b74ce4cfe67429d
hareesh-r <hareeshprogrammer@gmail.com>
2021-05-10 19:13:28 +0530

    Home Page

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from "./pages/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hello Google</h1>
+      <h3>Naan dhan da inimel uh vandhu ninna darbar uh</h3>
+      <Home />
     </div>
   );
 }
