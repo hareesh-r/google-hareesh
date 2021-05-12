6beb666f67781c3cdcec1d1e2fa25fabac8960ea
hareesh-r <hareeshprogrammer@gmail.com>
2021-05-13 00:01:53 +0530

    data layer set

--- google-clone/src/index.js
+++ google-clone/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducer , { initialState } from "./reducer";
+import { StateProvider } from "./StateProvider";
 
 ReactDOM.render(
   <React.StrictMode>
--- google-clone/src/pages/Search.js
+++ google-clone/src/pages/Search.js
@@ -4,15 +4,23 @@ import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
 import { useHistory } from "react-router-dom";
+import { useStateValue } from "../StateProvider";
+import { actionTypes } from "../reducer";
 
 
 function Search({ hideButtons = false }) {
+
+    const [{},dispatch] = useStateValue();
 
     const [ input , setInput] = useState("");
     const history = useHistory();
     const search = (e) => {
         e.preventDefault();
         console.log(input);
+        dispatch ({
+            type : actionTypes.SET_SEARCH_TERM,
+            term: input,
+        })
         history.push("/search");
     }
 
