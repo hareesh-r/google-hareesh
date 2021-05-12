6ac4290bf7d52b07805fa89070e8096ed0ab042d
hareesh-r <hareeshprogrammer@gmail.com>
2021-05-13 01:45:47 +0530

    dark mode optimization

--- google-clone/src/pages/SearchPage.js
+++ google-clone/src/pages/SearchPage.js
@@ -6,6 +6,11 @@ import Response from "../response";
 import { Link } from "react-router-dom";
 import Search from './Search';
 import SearchIcon from '@material-ui/icons/Search';
+import DescriptionIcon from '@material-ui/icons/Description';
+import ImageIcon from '@material-ui/icons/Image';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import RoomIcon from '@material-ui/icons/Room';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 
 function SearchPage() {
@@ -24,11 +29,45 @@ function SearchPage() {
                 </Link>
             </div>
             <div className="searchPage__Headerbody">
-                <Search hideButtons/>
+                <Search hideButtons />
                 <div className="searchPage__options">
                     <div className="searchPage__optionsLeft">
-                        <SearchIcon />
-                        <Link to="/all">All</Link >
+                        <div className="searchPage__option">
+                            <SearchIcon />
+                            <Link to="/all">All</Link >
+                        </div>
+                        <div className="searchPage__option">
+                            <DescriptionIcon />
+                            <Link to="/news">News</Link >
+                        </div>
+                        <div className="searchPage__option">
+                            <ImageIcon />
+                            <Link to="/images">Images</Link >
+                        </div>
+                        <div className="searchPage__option">
+                            <RoomIcon />
+                            <Link to="/shopping">Shopping</Link >
+                        </div>
+                        <div className="searchPage__option">
+                            <LocalOfferIcon />
+                            <Link to="/maps">Maps</Link >
+                        </div>
+                        <div className="searchPage__option">
+                            <MoreVertIcon />
+                            <Link to="/more">More</Link >
+                        </div>
+                    </div>
+                    <div className="searchPage__optionsRight">
+                        <div className="searchPage__option">
+                            <Link to="/setings">
+                                Settings
+                        </Link>
+                        </div>
+                        <div className="searchPage__option">
+                            <Link to="/tools">
+                                Tools
+                        </Link>
+                        </div>
                     </div>
                 </div>
             </div>
